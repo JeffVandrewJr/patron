1646761490c8a9ab2a7376bfedf7e143f3f3a9ac
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2018-12-28 14:37:19 -0500

    troubleshoot reset password flow

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -144,4 +144,4 @@ def reset_password(token):
         db.session.commit()
         flash('Your password has been reset.')
         return redirect(url_for('auth.login'))
-    return render_template('reset_password.html', form=form)
+    return render_template('auth/reset_password.html', form=form)
