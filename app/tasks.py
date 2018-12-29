32d3d4cecbc8ff59e379e3b72f090937f613e1db
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2018-12-29 18:01:47 -0500

    fix SQL language

--- app/tasks.py
+++ app/tasks.py
@@ -18,15 +18,15 @@ if minute is not None:
 def renewals():
     yesterday = datetime.today() - timedelta(hours=24)
     tomorrow = datetime.today() + timedelta(hours=24)
-    last_reminder = User.query.filter_by(
-        expiration < tomorrow,
-        expiration > yesterday
+    last_reminder = User.query.filter(
+        User.expiration < tomorrow,
+        User.expiration > yesterday
     ).all()
     six = datetime.today() + timedelta(hours=144)
     four = datetime.today() + timedelta(hours=96)
-    first_reminder = User.query.filter_by(
-        expiration < six,
-        expiration > four
+    first_reminder = User.query.filter(
+        User.expiration < six,
+        User.expiration > four
     ).all()
     reminder_list = first_reminder + last_reminder
     send_reminder_emails(scheduler.app, reminder_list)
