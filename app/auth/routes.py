e5f77a4fddb860aa69857ed033231a64f5cb4eb5
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2018-12-30 10:51:47 -0500

    typo in route

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -15,7 +15,7 @@ from datetime import date, timedelta
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('main.account'))
+        return redirect(url_for('auth.account'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
