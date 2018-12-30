37c83f48e18f83f8ea3920a52cf0b34176aaf8a9
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2018-12-29 23:51:02 -0500

    modifications for docker

--- config.py
+++ config.py
@@ -18,7 +18,8 @@ class Config(object):
     BLOGGING_ALLOW_FILE_UPLOAD = True
     BLOGGING_ESCAPE_MARKDOWN = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
+    if os.environ.get('MAIL_PORT') is not None:
+        MAIL_PORT = int(os.environ.get('MAIL_PORT'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
