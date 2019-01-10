d52c5cf0cae60bbd4a9d7cb9fbcf62ce9111791d
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2019-01-10 18:31:51 -0500

    add shell context

--- patron.py
+++ patron.py
@@ -3,7 +3,7 @@
 from app import create_app, db, blog_engine
 from app.email import send_reminder_emails
 from app.models import User, BTCPayClientStore, PriceLevel, \
-        ThirdPartyServices
+        ThirdPartyServices, Email
 from datetime import datetime, timedelta
 from flask_blogging_patron.signals import editor_post_saved
 
@@ -15,12 +15,15 @@ if __name__ == '__main__':
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User,
-            'editor_post_saved': editor_post_saved,
-            'blog_engine': blog_engine,
-            'BTCPayClientStore': BTCPayClientStore,
-            'PriceLevel': PriceLevel,
-            'send_reminder_emails': send_reminder_emails,
-            'tomorrow': datetime.today() + timedelta(hours=24),
-            'yesterday': datetime.today() - timedelta(hours=24),
-           }
+    return {
+        'db': db, 'User': User,
+        'editor_post_saved': editor_post_saved,
+        'blog_engine': blog_engine,
+        'Email': Email,
+        'ThirdPartyServices': ThirdPartyServices,
+        'BTCPayClientStore': BTCPayClientStore,
+        'PriceLevel': PriceLevel,
+        'send_reminder_emails': send_reminder_emails,
+        'tomorrow': datetime.today() + timedelta(hours=24),
+        'yesterday': datetime.today() - timedelta(hours=24),
+    }
