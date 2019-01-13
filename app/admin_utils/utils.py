b5f2350277703c4c5a9831e788ba45fe12d267e5
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2019-01-13 01:11:39 -0500

    isso cleanup

--- app/admin_utils/utils.py
+++ app/admin_utils/utils.py
@@ -1,4 +1,4 @@
-from app.models import Email, User, ThirdPartyServices
+from app.models import ThirdPartyServices
 from configparser import ConfigParser
 from flask import current_app
 import os
@@ -8,30 +8,13 @@ def isso_config():
     file = '/var/lib/config/isso.cfg'
     if os.path.isfile(file):
         os.remove(file)
-    email = Email.query.first()
     isso_pass = ThirdPartyServices.query.filter_by(
         name='isso').first().code
     isso_config = ConfigParser()
     isso_config['general'] = {}
     isso_config['general']['dbpath'] = '/var/lib/db/comments.db'
-    isso_config['general']['host'] = current_app.config['SERVER_NAME']
-    isso_config['general']['notify'] = 'smtp'
-    isso_config['general']['log-file'] = 'var/lib/db/moderation-queue.txt'
-    isso_config['admin'] = {}
+    isso_config['general']['host'] = current_app.config['BLOGGING_SITEURL']
     isso_config['admin']['enabled'] = 'true'
     isso_config['admin']['password'] = isso_pass
-    isso_config['moderation'] = {}
-    isso_config['moderation']['enabled'] = 'true'
-    isso_config['moderation']['purge-after'] = '30d'
-    isso_config['smtp'] = {}
-    isso_config['smtp']['username'] = email.username
-    isso_config['smtp']['password'] = email.password
-    isso_config['smtp']['host'] = email.server
-    isso_config['smtp']['port'] = str(email.port)
-    isso_config['smtp']['security'] = 'starttls'
-    isso_config['smtp']['to'] = User.query.filter_by(
-        role='admin').first().email
-    isso_config['smtp']['from'] = email.outgoing_email
-    isso_config['smtp']['timeout'] = '10'
     with open(file, 'w') as configfile:
         isso_config.write(configfile)
