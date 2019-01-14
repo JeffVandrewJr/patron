362e0fce908421582ed784266f148878c9841379
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2019-01-14 16:15:52 -0500

    load functions

--- patron.py
+++ patron.py
@@ -6,10 +6,36 @@ from app.models import User, BTCPayClientStore, PriceLevel, \
         ThirdPartyServices, Email
 from datetime import datetime, timedelta
 from flask_blogging_patron.signals import editor_post_saved
+import requests
+import threading
+import time
 
 app = create_app()
 
+
+def load_config():
+    def start_loop():
+        url = 'https://' + str(app.config['SERVER_NAME'])
+        while True:
+            try:
+                r = requests.get(url)
+                if r.status_code == 200:
+                    app.logger.info('Config successfully loaded.')
+                    break
+                else:
+                    app.config.info('Waiting to load config.')
+            except Exception:
+                app.logger.info('Waiting for site to load.')
+            time.sleep(2)
+    thread = threading.Thread(target=start_loop)
+    thread.start
+
+
+load_config()
+
+
 if __name__ == '__main__':
+    load_config()
     app.run(load_dotenv=True, ssl_context='adhoc')
 
 
