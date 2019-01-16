874168cc26519aa8012eb236b1bbfc41144615c0
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2019-01-15 19:24:18 -0500

    troubleshoot email

--- app/email.py
+++ app/email.py
@@ -1,5 +1,5 @@
 from app import mail
-from app.models import User
+from app.models import User, Email
 from datetime import datetime
 from flask import render_template, current_app, url_for
 from flask_mail import Message
@@ -39,6 +39,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
 
 def send_reminder_emails(app, reminder_list):
     with app.app_context():
+        email = Email.query.first()
         try:
             site = app.config['BLOGGING_SITENAME']
             with mail.connect() as conn:
@@ -51,7 +52,7 @@ def send_reminder_emails(app, reminder_list):
                     expires = user.expiration.date()
                     msg = Message(
                         f'{site} Renewal',
-                        sender=app.config.get('ADMIN'),
+                        sender=email.outgoing_email,
                         recipients=[user.email],
                         body=render_template(
                             'email/reminder.txt',
@@ -72,12 +73,13 @@ def send_failed_emails(app, failed_list, declined_list):
     with app.app_context():
         site = app.config['BLOGGING_SITENAME']
         url = url_for('main.support')
+        email = Email.query.first()
         with mail.connect() as conn:
             for user in failed_list:
                 expires = user.expiration.date()
                 msg = Message(
                     f'{site} Subscription Update',
-                    sender=app.config.get('ADMIN'),
+                    sender=email.outgoing_email,
                     recipients=[user.email],
                     body=render_template(
                         'email/reminder_cc.txt',
@@ -93,7 +95,7 @@ def send_failed_emails(app, failed_list, declined_list):
                 expires = user.expiration.date()
                 msg = Message(
                     f'{site} Card Declined',
-                    sender=app.config.get('ADMIN'),
+                    sender=email.outgoing_email,
                     recipients=[user.email],
                     body=render_template(
                         'email/cc_declined.txt',
@@ -121,7 +123,7 @@ def send_password_reset_email(user):
     email = Email.query.first()
     send_email(
         'Password Reset',
-        sender=email.outgoing_email
+        sender=email.outgoing_email,
         recipients=[user.email],
         text_body=render_template('email/reset_password.txt',
                                   user=user, token=token),
@@ -146,7 +148,7 @@ def email_post(post):
         users = User.query.filter_by(mail_opt_out=False).all()
         send_bulkmail(
             f'New Update from {site}',
-            sender=email.outgoing_email
+            sender=email.outgoing_email,
             users=users,
             html_body=html_body,
             text_body=text_body
