bef85ca134fd2832b7ff91998528beb9fa0d19e5
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2019-05-06 18:22:08 -0400

    fix scheduler test

--- tests/functional/test_scheduler.py
+++ tests/functional/test_scheduler.py
@@ -1,12 +1,16 @@
 from flask_ezmail.connection import email_dispatched
+from threading import Thread
+from time import sleep
 
 
-def test_scheduler(test_client, new_user):
+def test_scheduler(test_client, new_user, init_database):
     messages = []
 
     @email_dispatched.connect
     def suppressed_mail(message):
         messages.append(message)
 
+    from app import tasks
+    sleep(65)
     message = messages[0]
-    assert message.recipients == [new_user]
+    assert message.recipients == [new_user.email]
